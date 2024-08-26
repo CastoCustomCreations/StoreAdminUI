160a1fb21eb2bd0629affd53e4a336f6ec540c83
Jennifer Casto <CastoCustomCreations@gmail.com>
2024-08-26 10:08:10 -0400

    view-products-table

--- src/app/components/view-edit-products/view-edit-products.component.ts
+++ src/app/components/view-edit-products/view-edit-products.component.ts
@@ -4,10 +4,11 @@ import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-view-edit-products',
   standalone: true,
-  imports: [CommonModule,  MatCardModule, ReactiveFormsModule, HttpClientModule ],
+  imports: [CommonModule,  MatCardModule, ReactiveFormsModule, HttpClientModule,MatTableModule ],
   templateUrl: './view-edit-products.component.html',
   styleUrl: './view-edit-products.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -31,7 +32,7 @@ export class ViewEditProductsComponent {
     if (this.productForm.valid) {
       this.http.post('http://localhost:5050/api/Products', this.productForm.value)
         .subscribe(response => {
-          console.log('Customer data sent successfully:', response);
+          console.log('Product data saved successfully:', response);
         }, error => {
           console.error('Error sending customer data:', error);
         });
